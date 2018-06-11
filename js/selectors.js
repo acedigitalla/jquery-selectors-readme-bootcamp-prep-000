a165cd489afd02d0373543ed284ec184252d2f78
Alex Edwards <info@acedigitalagency.net>
2018-06-11 18:38:43 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+Function paragraphSelector()
+$('p')
+return
+
+Function lastImageSelector()
+$('img:last')
+return
+
+Function ninjaBabySelector()
+$('#baby-ninja')
+return
+
+Function divSelector()
+$('.pics')
+return
+
+Function firstListItem()
+$('#pic-list li:first-child')
+return
